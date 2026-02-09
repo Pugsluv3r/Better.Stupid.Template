9db4ed195906297c69b6cc097274fd7c2167a22d
Joergen <Nicetrybucko@nicetry.com>
2026-02-08 22:33:02 -0500

    test

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -3,7 +3,7 @@
     public class PluginInfo
     {
         public const string GUID = "org.iidk.gorillatag.menutemplate";
-        public const string Name = "ii's Stupid Template";
+        public const string Name = "ii's Stupid Template"; //this changes the title of your menu
         public const string Description = "Created by @goldentrophy with love <3";
         public const string Version = "1.9.5";
     }
